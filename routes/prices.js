4a22672436ad64663308ce0e790bc46bafc33022
Robert Guo <robertguo@me.com>
2014-01-21 14:55:52 -0800

    add mongosse

--- routes/prices.js
+++ routes/prices.js
@@ -2,7 +2,8 @@ var mongo = require('mongodb');
 var https = require('https');
   
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/strollnpi');
+var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/strollnpi');
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log("opened db");
